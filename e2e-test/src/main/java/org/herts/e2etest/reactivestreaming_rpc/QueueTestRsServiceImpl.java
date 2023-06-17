fe814202d9ba4851cf123762fd4eb58008cb16f6
tomoyane <tomoyane111@gmail.com>
2023-06-17 12:02:51 +0900

    refactor test pkg

--- e2e-test/src/main/java/org/herts/e2etest/reactivestreaming_rpc/QueueTestRsServiceImpl.java
+++ e2e-test/src/main/java/org/herts/e2etest/reactivestreaming_rpc/QueueTestRsServiceImpl.java
@@ -5,6 +5,7 @@ import org.herts.core.service.HertsServiceReactiveStreaming;
 public class QueueTestRsServiceImpl extends HertsServiceReactiveStreaming<QueueTestRsService, QueueTestRsReceiver> implements QueueTestRsService {
     @Override
     public void callFoo(String uniqId) {
+        System.out.println("Called " + uniqId);
         String clientId = getClientId();
         broadcast(clientId).onReceived(uniqId, clientId);
     }
